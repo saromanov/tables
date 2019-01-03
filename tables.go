e7b1f34c73dc05c605f46d1c8e0f5242e7f30770
Sergey <xxsmotur@gmail.com>
2019-01-04 02:33:13 +0500

    init of the project

---
+++ tables.go
@@ -0,0 +1,64 @@
+package tables
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+)
+
+// App is provides create of the new app
+type App struct {
+	writer *tabwriter.Writer
+}
+
+// New provides create of the new app
+func New() *App {
+	return &App{
+		writer: tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0),
+	}
+}
+
+// AddLine will write a new table line
+func (t *App) AddLine(args ...interface{}) {
+	formatString := t.buildFormatString(args)
+	fmt.Fprintf(t.writer, formatString, args...)
+}
+
+// AddHeader will write a new table line followed by a seperator
+func (t *App) AddHeader(args ...interface{}) {
+	t.AddLine(args...)
+	t.addSeperator(args)
+}
+
+// Print will write the table to the terminal
+func (t *App) Print() {
+	t.writer.Flush()
+}
+
+// addSeperator will write a new dash seperator line based on the args length
+func (t *App) addSeperator(args []interface{}) {
+	var b bytes.Buffer
+	for idx, arg := range args {
+		length := len(fmt.Sprintf("%v", arg))
+		b.WriteString(strings.Repeat("-", length))
+		if idx+1 != len(args) {
+			b.WriteString("\t")
+		}
+	}
+	fmt.Fprintln(t.writer, b.String())
+}
+
+// buildFormatString will build up the formatting string used by the *tabwriter.Writer
+func (t *App) buildFormatString(args []interface{}) string {
+	var b bytes.Buffer
+	for idx := range args {
+		b.WriteString("%v")
+		if idx+1 != len(args) {
+			b.WriteString("\t")
+		}
+	}
+	b.WriteString("\n")
+	return b.String()
+}
